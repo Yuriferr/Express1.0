01eeafeec1a1e7c7f180028cd2a315a5966c54ac
Yuriferr <104935046+Yuriferr@users.noreply.github.com>
2022-08-02 08:30:01 -0300

    Update bd.js

--- models/bd.js
+++ models/bd.js
@@ -3,7 +3,8 @@ const dados = {
     sobrenome: "Fernandes",
     idade: "17",
     descricao_projeto: "Eu queria aprender essa materia então fiz o projeto sozinho",
-    agradecimentos: "Muito Obrigado Sandra por fazer eu perder tempo das minhas férias fazendo esse trabalho maravilhoso 😡​"
+    agradecimentos: "Muito Obrigado Sandra por fazer eu perder tempo das minhas férias fazendo esse trabalho maravilhoso 😡​",
+    titleAPI: "API de pokémon"
 }
 
 module.exports = dados;
